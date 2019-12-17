f31ba704de3daa0aa62a133fb2967ee433c86bea
Johan Axfors <johan.axfors@renzgroup.se>
2019-12-17 09:10:55 +0100

    -Corrected intendtion

--- candelapy_web.py
+++ candelapy_web.py
@@ -8,28 +8,27 @@ from flask import request
 adapter = pygatt.GATTToolBackend()
 
 @app.route("/yeelight")
-def on():
+	def on():
 
- adapter.start()
- 
- intensity = int(request.args.get('intensity'))
+	adapter.start()
 
-  try:
-   print("Trying to connect...")
-   
-   device = adapter.connect("F8:24:41:C0:71:A7")
-   
-   if intensity > 0:
-    device.char_write_handle(0x001f, bytearray([0x43, 0x40, 0x01])) #on
-    device.char_write_handle(0x001f, bytearray([0x43, 0x42, intensity])) #intensiy
-   else:
-    device.char_write_handle(0x001f, bytearray([0x43, 0x40, 0x02])) #off
- except:
-  print("Connection Error")
+	intensity = int(request.args.get('intensity'))
 
- adapter.stop()
- 
- return "OK"
+	try:
+		print("Trying to connect...")
+		device = adapter.connect("F8:24:41:C0:71:A7")
+
+	if intensity > 0:
+		device.char_write_handle(0x001f, bytearray([0x43, 0x40, 0x01])) #on
+		device.char_write_handle(0x001f, bytearray([0x43, 0x42, intensity])) #intensiy
+	else:
+		device.char_write_handle(0x001f, bytearray([0x43, 0x40, 0x02])) #off
+	except:
+		print("Connection Error")
+
+	adapter.stop()
+
+	return "OK"
 
 if __name__ == "__main__":
- app.run(host='0.0.0.0',debug=True)
+app.run(host='0.0.0.0',debug=True)
