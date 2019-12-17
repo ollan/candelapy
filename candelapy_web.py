5c2718b97deebb50e23a9fb46d7ac7c18edc2a79
Johan Axfors <johan.axfors@renzgroup.se>
2019-12-17 09:28:01 +0100

    -Corrected intendtion

--- candelapy_web.py
+++ candelapy_web.py
@@ -16,14 +16,14 @@ def on():
 
  try:
   print("Trying to connect...")
-  device = adapter.connect("F8:24:41:C0:71:A7")
+  device = adapter.connect("F8:24:41:C2:FC:3F")
 
   if intensity > 0:
    device.char_write_handle(0x001f, bytearray([0x43, 0x40, 0x01])) #on
    device.char_write_handle(0x001f, bytearray([0x43, 0x42, intensity])) #intensiy
   else:
    device.char_write_handle(0x001f, bytearray([0x43, 0x40, 0x02])) #off
-   
+
  except:
   print("Connection Error")
 
